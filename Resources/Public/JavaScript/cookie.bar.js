54cadf7ffc3fa975391dc4746b8fa9a8ba55f60c
pascalfolleher <pascal.folleher@di-unternehmer.com>
2018-10-10 11:11:03 +0200

    Update to comply with GDPR when using google analytics

--- Resources/Public/JavaScript/cookie.bar.js
+++ Resources/Public/JavaScript/cookie.bar.js
@@ -4,12 +4,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 });
 
+var gaID = "ga-disable-" + window['ga-id'];
+
 function cookieDecision(element) {
+
     if (element.classList.contains('allow')) {
-        Cookies.set('allowCookies', 1, { expires: 365 });
+        Cookies.set('allowCookies', 1, { expires: 3650 });
+        window[gaID] = false;
     }
     if (element.classList.contains('forbid')) {
-        Cookies.set('allowCookies', 0, { expires: 365 });
+        Cookies.set('allowCookies', 0, { expires: 3650 });
+        window[gaID] = true;
     }
     document.getElementById('cookieBar').classList.add('cookiebar__hidden');
+}
+
+if (Cookies.get('allowCookies') === '1' || Cookies.get('allowCookies') === 1) {
+
+    window[gaID] = false;
+}
+
+// OptOut for impress link
+function gaOptOut() {
+    Cookies.set('allowCookies', 0, { expires: 3650 });
+    window[gaID] = true;
 }
